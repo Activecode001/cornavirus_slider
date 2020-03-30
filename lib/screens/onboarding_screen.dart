88885865b704197cdbbd0decf837af8fb3e44378
Activecode001 <activecode001@gmail.com>
2020-03-30 22:01:56 +0200

    update animated pages

--- lib/screens/onboarding_screen.dart
+++ lib/screens/onboarding_screen.dart
@@ -1,7 +1,7 @@
-import 'package:cornavirusslider/models/slide.dart';
-
-import '../widget/slide_item.dart';
 import 'package:flutter/material.dart';
+import 'dart:async';
+import '../models/slide.dart';
+import '../widget/slide_item.dart';
 
 class OnBoardingScreen extends StatefulWidget {
   @override
@@ -9,9 +9,40 @@ class OnBoardingScreen extends StatefulWidget {
 }
 
 class _OnBoardingScreenState extends State<OnBoardingScreen> {
+  int _currentPage = 0;
   final PageController _pageController = PageController(
     initialPage: 0,
   );
+
+  @override
+  void initState() {
+    super.initState();
+    Timer.periodic(Duration(seconds: 5), (Timer timer) {
+      if (_currentPage < 5) {
+        _currentPage++;
+      } else {
+        _currentPage = 0;
+      }
+      _pageController.animateToPage(
+        _currentPage,
+        duration: Duration(milliseconds: 300),
+        curve: Curves.easeIn,
+      );
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _pageController.dispose();
+  }
+
+  _onPageChanged(int index) {
+    setState(() {
+      _currentPage = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,6 +54,7 @@ class _OnBoardingScreenState extends State<OnBoardingScreen> {
         children: <Widget>[
           Expanded(
             child: PageView.builder(
+                onPageChanged: _onPageChanged,
                 controller: _pageController,
                 scrollDirection: Axis.horizontal,
                 itemCount: slideList.length,
